c1aa01fe08cdba1fd8573b32ae3ecfd5461a41ac
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-04-18 16:55:05 -0500

    Simplified firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:loggy/loggy.dart';
 
+import 'config/configuration.dart';
 import 'ui/my_app.dart';
 
 Future<void> main() async {
@@ -10,6 +12,18 @@ Future<void> main() async {
     logPrinter: const PrettyPrinter(
       showColors: true,
     ),
+  );
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: Configuration.apiKey,
+        authDomain: Configuration.authDomain,
+        databaseURL: Configuration.databaseURL,
+        projectId: Configuration.projectId,
+        storageBucket: Configuration.storageBucket,
+        messagingSenderId: Configuration.messagingSenderId,
+        appId: Configuration.appId,
+        measurementId: Configuration.measurementId),
   );
   runApp(MyApp());
 }
--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -8,63 +8,20 @@ import 'controllers/chat_controller.dart';
 import 'controllers/firestore_controller.dart';
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _init = Firebase.initializeApp(
-    options: FirebaseOptions(
-        apiKey: Configuration.apiKey,
-        authDomain: Configuration.authDomain,
-        databaseURL: Configuration.databaseURL,
-        projectId: Configuration.projectId,
-        storageBucket: Configuration.storageBucket,
-        messagingSenderId: Configuration.messagingSenderId,
-        appId: Configuration.appId,
-        measurementId: Configuration.measurementId),
-  );
-
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Get.put(FirestoreController());
+    Get.put(ChatController());
+    Get.put(AuthenticationController());
+
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Firebase demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Scaffold(
-            body: FutureBuilder(
-                future: _init,
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return const Wrong();
-                  }
-
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    // crear los controladores
-                    Get.put(FirestoreController());
-                    Get.put(ChatController());
-                    Get.put(AuthenticationController());
-                    return const FirebaseCentral();
-                  }
-
-                  return const Loading();
-                })));
-  }
-}
-
-class Wrong extends StatelessWidget {
-  const Wrong({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Error"));
-  }
-}
-
-class Loading extends StatelessWidget {
-  const Loading({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text("Loading"));
+        home: const FirebaseCentral());
   }
 }
